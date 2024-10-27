cf36024c899d4ace474f79d95de40b4e86d71c29
yeondub1121 <kssjshyjj@naver.com>
2024-10-27 18:42:34 +0900

    Section3: 스프링 웹 개발 기초

--- spring-study/src/main/java/spring/spring_study/controller/HelloController.java
+++ spring-study/src/main/java/spring/spring_study/controller/HelloController.java
@@ -2,6 +2,9 @@ package spring.spring_study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
@@ -12,4 +15,17 @@ public class HelloController {
         return "hello";
 
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //
+    public String helloString(@RequestParam("name") String name) {
+        return "bello " + name;
+    }
 }
+
